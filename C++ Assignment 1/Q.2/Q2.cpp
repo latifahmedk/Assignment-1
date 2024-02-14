ca7341a9684683db5b221b4607f3bc2b21e8affb
latifahmedk <153183321+latifahmedk@users.noreply.github.com>
2024-02-14 19:33:21 +0530

    Assignment 1

---
+++ C++ Assignment 1/Q.2/Q2.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int x=5;
+    int y=5;
+    int z=x+y;
+    cout<<z;
+    return 0;
+}
---
+++ C++ Assignment 1/Q.5/Q5.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int Radius=10;
+    cout<<"Radius of Cirle is : 10"<<endl;
+    int Circumfrence=2*3.1415*Radius;
+    cout<<"Circumfrence of Circle is : "<<Circumfrence;
+    return 0;
+}
